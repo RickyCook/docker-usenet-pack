4e71319e0aac590317ec335a852a80b6adc70e40
Ricky Cook <mail@thatpanda.com>
2014-02-14 19:56:28 +1100

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ ln -s "$CP_DIR/init/ubuntu" /etc/init.d/couchpotato
 
 # Link defaults, removing file if it already exists
 for file in $(ls $TOOL_DIR|grep default-); do
-	full_file=/etc/default/${$file:8}
+	full_file=/etc/default/${file:8}
 	[ -f $full_file ] && rm $full_file
 	ln -s $TOOL_DIR/$file $full_file
 done
