463951bf9abb3ff1a26cbe0a648c25397f557045
Ricky Cook <mail@thatpanda.com>
2014-02-14 19:23:27 +1100

    Link init script

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,19 @@
 #
 # Clone Sickbeard and CouchPotato
 # Checkout correct revisions
+# Link init scripts
 #
 
-cd /opt/
-git clone https://github.com/midgetspy/Sick-Beard.git sickbeard
-git clone https://github.com/RuudBurger/CouchPotatoServer.git couchpotato
+SB_DIR=/opt/sickbeard
+CP_DIR=/opt/couchpotato
 
-cd /opt/sickbeard/
+git clone https://github.com/midgetspy/Sick-Beard.git "$SB_DIR"
+git clone https://github.com/RuudBurger/CouchPotatoServer.git "$CP_DIR"
+
+cd "$SB_DIR"
 git checkout $SB_COMMIT
+ln -s "$SB_DIR/init.ubuntu" /etc/init.d/sickbeard
 
-cd /opt/couchpotato/
+cd "$CP_DIR"
 git checkout $CP_COMMIT
+ln -s "$CP_DIR/init/ubuntu" /etc/init.d/couchpotato
