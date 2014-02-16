c3607e2262ed0e0b396307fe7fd317c39be1e1c6
Ricky Cook <mail@thatpanda.com>
2014-02-16 22:00:01 +1100

    Add start of server edit menu

--- firstrun.py
+++ firstrun.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
-import locale, sys, traceback
+import locale, string, sys, traceback
 
-from cStringIO import StringIO
 from dialog import Dialog
 
 sys.path.extend((
@@ -9,8 +8,9 @@ sys.path.extend((
 ))
 
 from sabnzbd.config import read_config as sab_read_config
-from sabnzbd import cfg as sab_config, config as sab_config_options
+from sabnzbd import cfg as sab_config, config as sab_config_utils
 
+SAB_CONFIG = '/etc/sabnzbd/sabnzbd.ini'
 DIALOG_DEFAULTS = {'height': 8, 'width': 80}
 BIG_DIALOG_DEFAULTS = dict(DIALOG_DEFAULTS.items() + [('height', 35)])
 
@@ -32,8 +32,8 @@ def show_dialog(name, default, input_method, required=True, **kwargs):
 			return value
 
 
-def sab_config_question(name, field, default='', required=True, **kwargs):
-	if isinstance(field, sab_config_options.OptionPassword):
+def sab_config_question(name, field, default='', required=True, attr=None, **kwargs):
+	if isinstance(field, sab_config_utils.OptionPassword):
 		input_method = d.passwordbox
 		current_value = ''
 	else:
@@ -57,17 +57,44 @@ def sab_config_question(name, field, default='', required=True, **kwargs):
 	# Set the value
 
 
+def sab_server_menu(servers, action_text):
+	server_names = servers.keys()
+	server_choices = [(string.uppercase[idx], item) for idx, item in enumerate(server_names)]
+	kwargs = dict(BIG_DIALOG_DEFAULTS.items() + [
+		('height', min(BIG_DIALOG_DEFAULTS['height'], len(server_names) + 7))
+	])
+	cancelled, value = d.menu("%s which server?" % action_text, choices=server_choices, **kwargs)
+	return cancelled, servers[dict(server_choices)[value]]
+
 locale.setlocale(locale.LC_ALL, '')
 d = Dialog(dialog="dialog")
 d.setBackgroundTitle("Docker Usenet Pack - First Run")
 
 try:
 	# Load config
-	sab_read_config('/etc/sabnzbd/sabnzbd.ini')
+	sab_read_config(SAB_CONFIG)
 
-	section_defaults = dict(DIALOG_DEFAULTS.items() + [('title', 'UI Configuration')])
-	sab_config_question("Username", sab_config.username, "admin", True, **section_defaults)
-	sab_config_question("Password", sab_config.password, required=True, **section_defaults)
+	section_defaults = dict(DIALOG_DEFAULTS.items() + [('title', 'General Configuration')])
+	sab_config_question("UI Username", sab_config.username, "admin", True, **section_defaults)
+	sab_config_question("UI Password", sab_config.password, required=True, **section_defaults)
+
+	usenet_servers = sab_config_utils.get_servers()
+	if usenet_servers:
+		done = False
+		next_val = ("S", "Skip")
+		while not done:
+			big_section_defaults = dict(section_defaults.items() + [('height', 10)])
+			cancelled, value = d.menu("Servers already configured", choices=(("E", "Edit"), ("D", "Delete"), next_val), **big_section_defaults)
+
+			if value == 'E':
+				cancelled, server = sab_server_menu(usenet_servers, "Edit")
+			elif value == 'D':
+				cancelled, server = sab_server_menu(usenet_servers, "Delete")
+			next_val = ("O", "Done")
+	# set download_dir
+	# set complete_dir
+	# setup servers
+	# setup categories
 
 except Exception:
 	e_out = traceback.format_exc()
